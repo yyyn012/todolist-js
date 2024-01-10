3bcb6cbae624426a2db362b0ddb90f7202aae849
yyyn012 <111561826+yyyn012@users.noreply.github.com>
2024-01-10 15:26:02 +0900

    clock

--- js/greeting.js
+++ js/greeting.js
@@ -4,16 +4,11 @@ const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
-// 일반적으로 string만 포함된 변수는 대문자로 표기하고 string을 저장하고 싶을 때 사용한다.
-// 생성한 string을 반복해서 사용하게 될 경우 실수를 방지하려면 이렇게 변수로 저장해두는 것이 좋다.
 
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
-
   const username = loginInput.value;
-
-  // localStorage로 유저명 기억하기 - 첫 번째 인자로 저장될 값의 이름(key)과 값을 적어주면 된다.
   localStorage.setItem(USERNAME_KEY, username);
   paintGreetings(username);
 }
@@ -24,13 +19,11 @@ function paintGreetings(username) {
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
-// localStorage에 username이 없다면 null값을 반환한다.
 
 if (savedUsername === null) {
   loginForm.classList.remove(HIDDEN_CLASSNAME);
   loginForm.addEventListener("submit", onLoginSubmit);
-  // show the loginForm
 } else {
+  greeting.classList.remove(HIDDEN_CLASSNAME);
   paintGreetings(savedUsername);
-  // show the greetings
 }
